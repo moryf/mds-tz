3c822ddb35f5f0bc2d4e80312d71dba5823a5dfa
Moryf <aleksa.kb24@gmail.com>
2024-12-05 20:43:14 +0100

    Prvi commit

---
+++ src/main/java/com/mds/mdstzakcije/MdsTzAkcijeApplication.java
@@ -0,0 +1,13 @@
+package com.mds.mdstzakcije;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdsTzAkcijeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdsTzAkcijeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mds/mdstzakcije/MdsTzAkcijeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mds.mdstzakcije;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdsTzAkcijeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
